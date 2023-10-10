ef674121af79f1ca1dd03bd064a01b9508d67954
justin connors <justinconnors@hotmail.ca>
2023-10-10 15:02:31 -0300

    footer made

--- e-commerce/client/src/components/Footer/index.js
+++ e-commerce/client/src/components/Footer/index.js
@@ -1,10 +1,27 @@
-import react from "react";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { Box } from "@mui/material";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <Box
+      color="inherit"
+      sx={{ p: 1, m: 0.125, position: "fixed", bottom: 0, width: "100%" }}
+      component="footer"
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body2" color="inherit" align="center">
+          {"Copyright © "}
+          <Link color="inherit" href="https://github.com/Justin-Connors">
+            Justin Connors
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
