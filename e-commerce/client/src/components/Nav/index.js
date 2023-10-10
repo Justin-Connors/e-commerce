1654cbedc5bba3ee5c342b33d31f1e0f02f20474
justin connors <justinconnors@hotmail.ca>
2023-10-10 13:55:56 -0300

    got rid of nav/drawer

--- e-commerce/client/src/components/Nav/index.js
+++ e-commerce/client/src/components/Nav/index.js
@@ -1,63 +1,11 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import SideBar from "../Drawer";
+import react from "react";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const loggedIn = false;
-
-export default function Nav() {
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+function Nav() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="inherit">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={handleDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Wismazon
-          </Typography>
-          {loggedIn ? (
-            <Button color="inherit">Logout</Button>
-          ) : (
-            <Button color="inherit">Login</Button>
-          )}
-        </Toolbar>
-      </AppBar>
-      <SideBar open={open} onClose={handleDrawerClose} />
+    <div>
+      <h1>Nav</h1>
     </div>
   );
 }
+
+export default Nav;
