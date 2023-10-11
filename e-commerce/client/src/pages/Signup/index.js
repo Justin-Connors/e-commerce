52df99ad37920b6f2a9f117a464634a2c0b5b84e
justin connors <justinconnors@hotmail.ca>
2023-10-11 14:34:47 -0300

    signup working

--- e-commerce/client/src/pages/Signup/index.js
+++ e-commerce/client/src/pages/Signup/index.js
@@ -1,14 +1,69 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../../utils/auth";
+import { ADD_USER } from "../../utils/mutations";
+import validator from "validator";
 import { TextField, Button, Typography } from "@mui/material";
 
-function Signup() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function Signup(props) {
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+  const [blockSubmit, setBlockSubmit] = useState(true);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  useEffect(() => {
+    document.title = props.title;
+  }, [props.title]);
+
+  useEffect(() => {
+    const { firstName, lastName, email, password } = formState;
+    const isEmail = validator.isEmail(email);
+    const isPassword = validator.isStrongPassword(password, {
+      min: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    });
+    if (firstName && lastName && isEmail && isPassword) {
+      setBlockSubmit(false);
+    } else {
+      setBlockSubmit(true);
+    }
+  }, [formState]);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    // handle form submission
+    try {
+      e.preventDefault();
+      const mutationResponse = addUser({
+        variables: {
+          firstName: formState.firstName,
+          lastName: formState.lastName,
+          email: formState.email,
+          password: formState.password,
+        },
+      });
+      const token = mutationResponse.data.addUser.token;
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState({ ...formState, [name]: value });
+    if (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -17,25 +72,44 @@ function Signup() {
         Sign Up
       </Typography>
       <TextField
-        label="Username"
+        label="First Name"
+        name="firstName"
+        type="firstName"
         variant="outlined"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        value={formState.firstName}
+        onInput={handleChange}
+      />
+      <TextField
+        label="Last Name"
+        name="lastName"
+        type="lastName"
+        variant="outlined"
+        value={formState.lastName}
+        onInput={handleChange}
       />
       <TextField
         label="Email"
+        name="email"
+        type="email"
         variant="outlined"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={formState.email}
+        onInput={handleChange}
       />
       <TextField
         label="Password"
+        name="password"
         variant="outlined"
         type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        helperText="Password must be at least 8 characters and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character"
+        value={formState.password}
+        onInput={handleChange}
       />
-      <Button variant="outlined" color="inherit" type="submit">
+      <Button
+        variant="outlined"
+        color="inherit"
+        type="submit"
+        disabled={blockSubmit}
+      >
         Sign up
       </Button>
     </form>
